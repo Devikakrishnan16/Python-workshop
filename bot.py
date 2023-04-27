e8dba5b069bd9d12e311bc3499d96438f75911ce
Devikakrishnan16 <130470769+Devikakrishnan16@users.noreply.github.com>
2023-04-27 18:30:16 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,33 @@
 import asyncio
 from config import *
 from telethon import TelegramClient ,events
+import requests
+from bs4 import BeautifulSoup
+
+def scrape(url):
+    file_name = "books.txt"
+    req = requests.get(url)
+    source_code = req.content
+
+    soup = BeautifulSoup(source_code , 'lxml')
+
+    articles = soup.find_all('article')
+    for article in articles:
+        h3 = article.find('h3')
+        a = (h3.find('a')).text
+        price = article.find('div' , class_= "product_price")
+        price= price.find('p')
+        price = price.text[1:]
+        print(type(price))
+        price = float(price)
+        print(type(price))
+        print(price)
+        if price <50:
+           with open(file_name, 'a') as file:
+                file.write(a + ":" + str(price) + '\n')
+            
+    return file_name
+
 
 async def main():
     #main function
@@ -9,5 +36,13 @@ async def main():
     )
     async with bot :
         print( "Logged in")
+        @bot.on(events.NewMessage())#decorators
+        async def handle_message(event):
+            link = event.message.message
+            file = scrape(link)
+            user_id = event.sender.id
+            await bot.send_file(user_id, file)
+        await bot.run_until_disconnected()
+
 
 asyncio.run(main())        
